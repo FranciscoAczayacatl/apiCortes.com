e312875ae4f6bd1cb6909d79632a23490eb47e89
FranciscoAczayacatl <franciscoaczayacatlg@gmail.com>
2023-09-05 19:48:58 -0600

    feturees database

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,14 +2,11 @@ const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
 
-const db = new Sequelize({
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
 
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  dialect: 'postgres',
+  dialect: 'mysql',
   logging: false,
   });
 
